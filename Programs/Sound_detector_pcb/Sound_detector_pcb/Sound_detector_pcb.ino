a6c7a90a239899d07511a5da8652bd304a594539
WayTechEng <70178222+WayTech-Eng@users.noreply.github.com>
2021-05-03 14:11:23 +1000

    Added LCD to sound detector program

--- Programs/Sound_detector_pcb/Sound_detector_pcb/Sound_detector_pcb.ino
+++ Programs/Sound_detector_pcb/Sound_detector_pcb/Sound_detector_pcb.ino
@@ -1,47 +1,84 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "DS1307.h"
+
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0 , 4, 5, 6, 7, 3, POSITIVE);
+
 int led = 6;
 int sound_digital = 7;
 int sound_analog = A0;
-int sum = 0;
+long int sum = 0;
 int counter = 0;
 int max_val = 0;
+int arr[800] = {0};
+int num = 150;
+DS1307 clock;//define a object of DS1307 class
 
 void setup(){
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   pinMode(sound_digital, INPUT);  
+
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.print("Hello");
+  lcd.setCursor( 0, 1 );
+  delay(500);
+  lcd.clear();
+  lcd.print("SOUND DETECTOR");
+  lcd.setCursor( 0, 1 );
+  lcd.print("B.WAYMOUTH");
+  delay(500);
 }
 
-void loop(){
-  int val_digital = digitalRead(sound_digital);
-  int val_analog = analogRead(sound_analog);
-  sum += val_analog;
-  counter++;
-  if(val_analog > max_val)
+void loop()
+{
+  for(int i = 0; i < num; i++)
   {
-    max_val = val_analog;
+    int val_analog = analogRead(sound_analog);
+    sum += val_analog;
   }
+  int level = sum / num;
+//  Serial.println(level);
+  sum = 0;
+  lcd.clear();
+  lcd.print(level);
+  delay(100);
+}
 
-  if(counter >= 1000)
-  {
-    int avg = sum/counter;
-//    Serial.println(val_analog);
-//    Serial.println(max_val);
-    counter = 0;
-    sum = 0;
-    max_val = 0;
-  }
-
-  Serial.println(val_analog);
-//  Serial.println("\t");
-//  Serial.println(val_digital);
+//void loop(){
+//  int val_digital = digitalRead(sound_digital);
+//  int val_analog = analogRead(sound_analog);
+//  sum += val_analog;
+//  counter++;
+//  if(val_analog > max_val)
+//  {
+//    max_val = val_analog;
+//  }
 //
-//  if (val_digital == HIGH)
+//  if(counter >= 1000)
 //  {
-//    digitalWrite (led, HIGH);
-//    delay(3000);
-//    }
-//  else
-//  {
-//    digitalWrite (led, LOW);
-//    }
-}
+//    int avg = sum/counter;
+////    Serial.println(val_analog);
+////    Serial.println(max_val);
+////    Serial.println(avg);
+//    counter = 0;
+//    sum = 0;
+//    max_val = 0;
+//  }
+//
+//  Serial.println(val_analog);
+////  Serial.println("\t");
+////  Serial.println(val_digital);
+////
+////  if (val_digital == HIGH)
+////  {
+////    digitalWrite (led, HIGH);
+////    delay(3000);
+////    }
+////  else
+////  {
+////    digitalWrite (led, LOW);
+////    }
+//}
